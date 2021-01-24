7c957fcb83b4fad7aaf1b37892f279ac52d27769
rishabhamu <56648917+rishabhamu@users.noreply.github.com>
2021-01-24 18:02:47 +0530

    day3 assignment2 submitted

---
+++ assignment2/Q.2.js
@@ -0,0 +1,9 @@
+var arr = [12,23,34,45,56,67,78,89,90];
+for(var num=0; num < arr.length ; num++)
+{
+   
+    // console.log(arr[num]);
+    var sum = arr[num]+arr[num-1];
+  
+}
+console.log(sum);
